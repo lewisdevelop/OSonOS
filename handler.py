7f069076819c1e7e6764bf6da04381860fd1baf2
lewisdevelop <44608738+lewisdevelop@users.noreply.github.com>
2023-06-23 09:28:30 +0100

    First Commit

---
+++ handler.py
@@ -0,0 +1,4 @@
+def terminal(filepath):
+    runningApp = True
+    while runningApp:
+        terminalPrompt = input(f"{filepath}:>")
---
+++ main.py
@@ -0,0 +1,5 @@
+from handler import *
+
+appSelecter = input("Select an app to run:>")
+if appSelecter == "terminal":
+    terminal("/")
